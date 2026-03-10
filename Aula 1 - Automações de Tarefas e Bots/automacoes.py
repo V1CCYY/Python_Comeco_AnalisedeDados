5d417c3e9d6597fa3008c27b8c25bd2b108412ee
Victhor Gladistone <gladistone245@gmail.com>
2026-03-10 12:20:22 -0300

    Update automacoes.py

--- Aula 1 - Automações de Tarefas e Bots/automacoes.py
+++ Aula 1 - Automações de Tarefas e Bots/automacoes.py
@@ -1,14 +1,11 @@
 # Passo a passo do projeto
 # Passo 1: Entrar no sistema da empresa 
+    #Usado no Exemplo
     # https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
 import pyautogui
 import time
 
-# pyautogui.write -> escrever um texto
-# pyautogui.press -> apertar 1 tecla
-# pyautogui.click -> clicar em algum lugar da tela
-# pyautogui.hotkey -> combinação de teclas
 pyautogui.PAUSE = 0.3
 
 # abrir o navegador (chrome)
@@ -23,9 +20,12 @@ time.sleep(3)
 
 
 # Passo 2: Fazer login
+
 # selecionar o campo de email
+
 pyautogui.click(x=685, y=451)
 # escrever o seu email
+
 pyautogui.write("pythonimpressionador@gmail.com")
 pyautogui.press("tab") # passando pro próximo campo
 pyautogui.write("sua senha")
@@ -33,6 +33,7 @@ pyautogui.click(x=955, y=638) # clique no botao de login
 time.sleep(3)
 
 # Passo 3: Importar a base de produtos pra cadastrar
+
 import pandas as pd
 
 tabela = pd.read_csv("produtos.csv")
@@ -40,16 +41,26 @@ tabela = pd.read_csv("produtos.csv")
 print(tabela)
 
 # Passo 4: Cadastrar um produto
+
 for linha in tabela.index:
     # clicar no campo de código
+    
     pyautogui.click(x=653, y=294)
+    
     # pegar da tabela o valor do campo que a gente quer preencher
+    
     codigo = tabela.loc[linha, "codigo"]
+    
     # preencher o campo
+    
     pyautogui.write(str(codigo))
+    
     # passar para o proximo campo
+    
     pyautogui.press("tab")
+    
     # preencher o campo
+    
     pyautogui.write(str(tabela.loc[linha, "marca"]))
     pyautogui.press("tab")
     pyautogui.write(str(tabela.loc[linha, "tipo"]))
@@ -65,6 +76,8 @@ for linha in tabela.index:
         pyautogui.write(str(tabela.loc[linha, "obs"]))
     pyautogui.press("tab")
     pyautogui.press("enter") # cadastra o produto (botao enviar)
+    
     # dar scroll de tudo pra cima
     pyautogui.scroll(5000)
+    
     # Passo 5: Repetir o processo de cadastro até o fim
